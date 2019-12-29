6b9b90b72852063d2d148581040e3685dfbba9ac
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2019-12-29 10:13:24 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -11,7 +11,7 @@
 sed -i 's/192.168.1.1/12.0.0.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 添加LEAN软件包
-git clone https://github.com/coolsnowwolf/lede/trunk/package/lean package/lede
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean package/lede
 
 # 替换默认Argon主题
 # rm -rf package/lede/luci-theme-argon
