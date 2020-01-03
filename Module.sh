762ee1a378b87db439e2517d6f159495aeba975d
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-01-03 21:09:50 +0800

    Update Module.sh

--- Module.sh
+++ Module.sh
@@ -18,3 +18,26 @@ git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
+
+# 添加Lienol软件包
+sed -i '$a https://github.com/Lienol/openwrt-package' feeds.conf.default
+# git clone https://github.com/Lienol/openwrt-package package/openwrt-package（备用）
+
+# 删除shadowsocksr puls+
+./scripts/feeds clean
+./scripts/feeds update -a
+rm -rf feeds/lienol/lienol/v2ray
+rm -rf feeds/lienol/lienol/openssl1.1
+rm -rf feeds/lienol/lienol/trojan
+rm -rf feeds/lienol/lienol/ipt2socks
+rm -rf feeds/lienol/lienol/shadowsocks-libev-new
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/lienol/luci-app-verysync
+rm -rf package/lean/kcptun
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
+./scripts/feeds install -a
