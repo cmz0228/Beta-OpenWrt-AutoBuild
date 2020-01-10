0a7cf2b1be1d2a84ab3307dbaeb218c2fa292af0
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-01-10 14:30:32 +0800

    Update OpenWrt-x86-64.sh

--- OpenWrt-x86-64.sh
+++ OpenWrt-x86-64.sh
@@ -1,5 +1,10 @@
-  
+
 #!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
 #=================================================
 
 # 定制默认IP
