423bb4a343bbc3af7cc208e565d5201bb4800390
尐ˊ;栤嚸┌ <20243226+kang-mk@users.noreply.github.com>
2020-01-14 11:35:17 +0800

    Update and rename OpenWrt-x86-64.sh to Mod.sh

---
+++ Mod.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=================================================
+
+# 定制默认IP
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# 添加第三方软件包
+git clone https://github.com/KFERMercer/luci-app-serverchan package/luci-app-serverchan
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
